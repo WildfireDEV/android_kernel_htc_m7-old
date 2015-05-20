0b6a6835fe2abddc92e286c73a930057c5879e4d
varchild <sirstreak@gmail.com>
2015-05-20 10:49:12 -0500

    Patch 3.4.68
    Conflicts:
    	include/net/dst.h

--- include/net/dst.h
+++ include/net/dst.h
@@ -309,11 +309,6 @@ static inline void skb_dst_copy(struct sk_buff *nskb, const struct sk_buff *oskb
  */
 static inline void skb_dst_force(struct sk_buff *skb)
 {
-    if ((!skb) || (IS_ERR(skb))) {
-        printk("[NET] skb is NULL in %s\n", __func__);
-        return;
-    }
-
 	if (skb_dst_is_noref(skb)) {
 		WARN_ON(!rcu_read_lock_held());
 		skb->_skb_refdst &= ~SKB_DST_NOREF;
@@ -468,11 +463,23 @@ static inline struct dst_entry *xfrm_lookup(struct net *net,
 					    int flags)
 {
 	return dst_orig;
+} 
+
+static inline struct xfrm_state *dst_xfrm(const struct dst_entry *dst)
+{
+	return NULL;
 }
+
 #else
 extern struct dst_entry *xfrm_lookup(struct net *net, struct dst_entry *dst_orig,
 				     const struct flowi *fl, struct sock *sk,
 				     int flags);
+
+/* skb attached with this dst needs transformation if dst->xfrm is valid */
+static inline struct xfrm_state *dst_xfrm(const struct dst_entry *dst)
+{
+	return dst->xfrm;
+}
 #endif
 
 #endif /* _NET_DST_H */
